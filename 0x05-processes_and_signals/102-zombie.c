5f20f83007a876cdb1332495c6ffc0cadbc801b7
hollaze <deperiers.a@gmail.com>
2021-06-22 10:02:17 +0200

    checking

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * infinite_while - usefull to be able to see the zombies
+ * Return: 0
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - create 5 zombies to see them: ps aux | grep -e 'Z+.*<defunct>'
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			return (0);
+		}
+	}
+	infinite_while();
+}

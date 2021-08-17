1af9d957d28cc4d9316011deea8b4297362ca338
hollaze <deperiers.a@gmail.com>
2021-08-17 17:39:40 +0000

    modifying order

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 #Setting up the client SSH configuration file,
 #to connect to a server without typing a password
 
-file_line { 'Using private key':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentifyFile ~/.ssh/holberton',
-}
-
 file_line { 'Refuse authentificate password':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentification no',
+}
+
+file_line { 'Using private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentifyFile ~/.ssh/holberton',
 }

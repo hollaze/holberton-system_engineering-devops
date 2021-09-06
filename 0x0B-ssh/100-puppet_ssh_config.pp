165bc906c70e6be883b76f0313cd600536cbc028
hollaze <deperiers.a@gmail.com>
2021-09-06 05:15:30 +0200

    ensuring the file is present

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,13 @@
 #Setting up the client SSH configuration file
 
-file { 'File is present':
+file { 'ssh_config is present':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
+}
+
+file { 'holberton is present':
+  ensure => 'present',
+  path   => '~/.ssh/holberton',
 }
 
 file_line { 'PasswordAuthentication':

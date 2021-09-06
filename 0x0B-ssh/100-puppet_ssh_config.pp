88646c15f28d55dfdc12663b9dc6cd2fdfbd6484
hollaze <deperiers.a@gmail.com>
2021-09-06 04:20:35 +0200

    comments modif

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 #Setting up the client SSH configuration file
 
-file_line { 'Turn off passwd auth':
-  ensure => present,
+file_line { 'PasswordAuthentication':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file_line { 'Declare identity file':
-  ensure => present,
+file_line { 'IdentifyFile':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentifyFile ~/.ssh/holberton',
 }

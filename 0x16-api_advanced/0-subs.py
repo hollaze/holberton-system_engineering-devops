2e8c6f82033a95198f99fc51bfeeb126a6773745
hollaze <deperiers.a@gmail.com>
2021-09-28 17:33:43 +0200

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ catch number of subscriber from chosen subreddit """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit) # programming
+    url = 'https://www.reddit.com/r/{}/about.json'.format(
+        subreddit)  # programming
     user_agent = {'User-Agent': 'Holberton Agent'}
 
     r = requests.get(url, headers=user_agent, allow_redirects=False)

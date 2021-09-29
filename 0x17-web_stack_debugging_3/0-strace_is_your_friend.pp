4ac50ff1aa6ca269cfa6ff9ee3217a51a80dc8d4
hollaze <deperiers.a@gmail.com>
2021-09-29 12:06:14 +0200

    check

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,4 @@
+# Debugging
+exec { '/var/www/html/wp-setting.php':
+path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",}

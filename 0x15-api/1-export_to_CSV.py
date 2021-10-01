feb95241d85cf8e9773f414b757b01030d840305
hollaze <deperiers.a@gmail.com>
2021-10-01 19:30:43 +0200

    export data into csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ export data into csv file """
-import requests
-from sys import argv
 import csv
+import requests
+import sys
+
 
 if __name__ == "__main__":
     todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
-                         .format(argv[1]))
+                         .format(sys.argv[1]))
     name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(argv[1])).json()['name']
+                        .format(sys.argv[1])).json()['name']
 
-    with open('{}.csv'.format(argv[1]), 'w', newline='') as file_name:
+    with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as file_name:
         write = csv.writer(file_name, quoting=csv.QUOTE_ALL)
-        [write.writerow([argv[1], name, task['completed'], task['title']]
+        [write.writerow([sys.argv[1], name, task['completed'], task['title']]
                         ) for task in todos.json()]

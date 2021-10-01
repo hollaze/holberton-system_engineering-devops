e134db3807b6797aa2fd6aa042b9d934eba9b956
hollaze <deperiers.a@gmail.com>
2021-10-01 19:43:34 +0200

    name to username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,10 +8,10 @@ import sys
 if __name__ == "__main__":
     todos = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
                          .format(sys.argv[1]))
-    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(sys.argv[1])).json()['name']
+    username = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(sys.argv[1])).json()['username']
 
     with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as file_name:
         write = csv.writer(file_name, quoting=csv.QUOTE_ALL)
-        [write.writerow([sys.argv[1], name, task['completed'], task['title']]
+        [write.writerow([sys.argv[1], username, task['completed'], task['title']]
                         ) for task in todos.json()]

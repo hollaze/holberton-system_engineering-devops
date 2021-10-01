2553c5a1ec1690313a3e5212a79e921f1be67bc3
hollaze <deperiers.a@gmail.com>
2021-10-01 22:54:35 +0200

    export all data into json file

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" export data into json file from api """
+import json
+import requests
+
+if __name__ == "__main__":
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/").json()
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users/").json()
+
+    dict = {user['id']: [{
+        "task": task['title'],
+        "completed": task['completed'],
+        "username": user['username']} for task in todos] for user in users}
+
+    with open("todo_all_employees.json", 'w') as file:
+        json.dump(dict, file)

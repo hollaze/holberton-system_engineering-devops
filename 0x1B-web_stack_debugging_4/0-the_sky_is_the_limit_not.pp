efd6dc0ddce540c4bdd125bd779f57306b49ce35
hollaze <deperiers.a@gmail.com>
2021-10-04 19:54:50 +0200

    check

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increase requests limit
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+}
+
+exec { 'restart nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
+}

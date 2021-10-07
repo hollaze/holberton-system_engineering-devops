92543f4d1c958079a09934f41560034ab862d2cc
hollaze <deperiers.a@gmail.com>
2021-10-07 12:28:51 +0200

    test

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" etc/default/nginx',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/:/usr/local/bin/',
+  path    => '/bin/:/usr/local/bin/',
 }
 
 exec { 'restart nginx':

bc17ff141a1023de503f3db296b60cf34909a374
hollaze <deperiers.a@gmail.com>
2021-10-07 12:12:10 +0200

    modifying path

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,11 @@
 # with the holberton user and open a file without any error message
 
 exec { 'holberton-hard':
-	command => 'sed -i "/holberton hard nofile/s/5/97816/" /etc/security/limits.conf',
-	path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  command => 'sed -i "/holberton hard nofile/s/5/97816/" /etc/security/limits.conf',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/:/usr/local/bin/',
 }
 
 exec { 'holberton-soft':
-	command => 'sed -i "/holberton soft nofile/s/5/97816/" /etc/security/limits.conf',
-	path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  command => 'sed -i "/holberton soft nofile/s/5/97816/" /etc/security/limits.conf',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/:/usr/local/bin/',
 }
